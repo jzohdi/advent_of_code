03d931ce5dd3fb48f6cb80ad3f29e540f3085cb1
jzohdi <jakez1@live.com>
2021-12-02 12:30:42 +0100

    removed return statements

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn part_one() -> i32 {
-    return file_to_ints("./src/day_one/input.txt")
+    file_to_ints("./src/day_one/input.txt")
         .into_iter()
         .fold((0, None), |(acc, prev), curr| {
             if let Some(p) = prev {
@@ -11,20 +11,20 @@ pub fn part_one() -> i32 {
             }
             (acc, Some(curr))
         })
-        .0;
+        .0
 }
 
 pub fn part_two() -> i32 {
     let nums = file_to_ints("./src/day_one/input.txt");
 
-    return (3..nums.len()).fold(0, |acc, idx| {
+    (3..nums.len()).fold(0, |acc, idx| {
         if nums[idx - 2] + nums[idx - 1] + nums[idx] > nums[idx - 3] + nums[idx - 2] + nums[idx - 1]
         {
             acc + 1
         } else {
             acc
         }
-    });
+    })
 }
 
 fn file_to_ints(file: &str) -> Vec<i32> {
