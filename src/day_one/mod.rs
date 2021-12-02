a8bf86deab5f376e4d64c2df40fec9ce3818f6dc
jzohdi <jakez1@live.com>
2021-12-02 12:23:11 +0100

    finished day 2

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,20 +1,34 @@
 use std::fs;
 
-pub fn answer() {
-    println!(
-        "Answer: {}",
-        fs::read_to_string("./src/day_one/input.txt")
-            .unwrap()
-            .lines()
-            .map(|line| line.parse::<i32>().unwrap())
-            .fold((0, None), |(acc, prev), curr| {
-                if let Some(p) = prev {
-                    if p > curr {
-                        return (acc + 1, Some(curr));
-                    }
+pub fn part_one() -> i32 {
+    return fs::read_to_string("./src/day_one/input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .fold((0, None), |(acc, prev), curr| {
+            if let Some(p) = prev {
+                if p < curr {
+                    return (acc + 1, Some(curr));
                 }
-                (acc, Some(curr))
-            })
-            .0
-    )
+            }
+            (acc, Some(curr))
+        })
+        .0;
+}
+
+pub fn part_two() -> i32 {
+    let nums: Vec<i32> = fs::read_to_string("./src/day_one/input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect();
+
+    return (3..nums.len()).fold(0, |acc, idx| {
+        if nums[idx - 2] + nums[idx - 1] + nums[idx] > nums[idx - 3] + nums[idx - 2] + nums[idx - 1]
+        {
+            acc + 1
+        } else {
+            acc
+        }
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod day_one;
 // use crate::day_one;
 
 fn main() {
-    day_one::answer()
+    println!("Day 1, part one: {}", day_one::part_one());
+    println!("Day 2, part two: {}", day_one::part_two());
 }
