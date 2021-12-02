73af481a08d4f0fade586f6c2c870778c4d64f1e
jzohdi <jakez1@live.com>
2021-12-02 12:29:43 +0100

    function of reading lnes

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,10 +1,8 @@
 use std::fs;
 
 pub fn part_one() -> i32 {
-    return fs::read_to_string("./src/day_one/input.txt")
-        .unwrap()
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
+    return file_to_ints("./src/day_one/input.txt")
+        .into_iter()
         .fold((0, None), |(acc, prev), curr| {
             if let Some(p) = prev {
                 if p < curr {
@@ -17,11 +15,7 @@ pub fn part_one() -> i32 {
 }
 
 pub fn part_two() -> i32 {
-    let nums: Vec<i32> = fs::read_to_string("./src/day_one/input.txt")
-        .unwrap()
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect();
+    let nums = file_to_ints("./src/day_one/input.txt");
 
     return (3..nums.len()).fold(0, |acc, idx| {
         if nums[idx - 2] + nums[idx - 1] + nums[idx] > nums[idx - 3] + nums[idx - 2] + nums[idx - 1]
@@ -31,4 +25,12 @@ pub fn part_two() -> i32 {
             acc
         }
     });
+}
+
+fn file_to_ints(file: &str) -> Vec<i32> {
+    fs::read_to_string(file)
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect()
 }
