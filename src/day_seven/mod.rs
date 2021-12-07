21d7500b7dd21948ce416d70b420b45137aea2cc
jzohdi <jakez1@live.com>
2021-12-07 12:54:55 +0100

    day 7

---
+++ src/day_seven/mod.rs
@@ -0,0 +1,36 @@
+use std::cmp::{max, min};
+use std::fs;
+
+const FILE_PATH: &str = "./src/day_seven/input.txt";
+
+pub fn solution(guassian: bool) -> usize {
+    let input: Vec<usize> = fs::read_to_string(FILE_PATH)
+        .unwrap()
+        .trim()
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+    let length = input.len();
+    (0..length)
+        .map(|idx| calc_fuel_to_move_to(&input, idx, guassian))
+        .min()
+        .unwrap()
+}
+
+fn calc_fuel_to_move_to(input: &Vec<usize>, idx: usize, guassian: bool) -> usize {
+    return input
+        .into_iter()
+        .map(|val| {
+            if guassian {
+                guassian_sum(min(*val, idx), max(*val, idx))
+            } else {
+                max(*val, idx) - min(*val, idx)
+            }
+        })
+        .sum();
+}
+
+fn guassian_sum(lower: usize, upper: usize) -> usize {
+    let n = upper - lower;
+    (n * (n + 1)) / 2
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_five;
 mod day_one;
+mod day_seven;
 mod day_six;
 mod day_three;
 mod day_two;
@@ -15,4 +16,6 @@ fn main() {
     println!("Day 5, part two: {}", day_five::solution(true));
     println!("Day 6, part one: {}", day_six::make_fish(80));
     println!("Day 6, part two: {}", day_six::make_fish(256));
+    println!("Day 7, part one: {}", day_seven::solution(false));
+    println!("Day 7, part two: {}", day_seven::solution(true));
 }
