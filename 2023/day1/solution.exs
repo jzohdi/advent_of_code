d43489b5fd65372be89c82b6d7d678c8182644ba
jzohdi <jakez1@live.com>
2023-12-03 19:54:04 -0500

    fixed day1

--- 2023/day1/solution.exs
+++ 2023/day1/solution.exs
@@ -1,38 +1,24 @@
 defmodule Solution do
   def part_one do
+    num_map  = %{"one" => 1, "two" => 2, "three" =>  3, "four" =>  4, "five" =>  5, "six" =>  6, "seven" =>  7, "eight" => 8, "nine" => 9,
+     "1" =>  1, "2" =>  2, "3" =>  3, "4" =>  4, "5" =>  5, "6" =>  6, "7" =>  7, "8" =>  8 , "9" =>  9, "zero"=> 0, "0" =>  0}
     {:ok, contents} = File.read("./day1/input.txt")
     contents_to_lines = contents |> String.split("\n", trim: true)
-    # IO.inspect(contents_to_lines)
-    # IO.inspect(contents_to_lines)
-    # get_digits = for line <- contents_to_lines, do: Regex.scan(~r/\d/, line) |> IO.inspect()
-    # for line <- contents_to_lines, do: IO.inspect(line)
     sum = List.foldl(contents_to_lines, 0, fn(line, acc) ->
       # IO.inspect(x)
-      line = Regex.replace(~r/one/,line, "1")
-      line = Regex.replace(~r/two/, line, "2")
-      line = Regex.replace(~r/three/, line, "3")
-      line = Regex.replace(~r/four/, line, "4")
-      line = Regex.replace(~r/five/, line, "5")
-      line = Regex.replace(~r/six/, line, "6")
-      line = Regex.replace(~r/seven/, line, "7")
-      line = Regex.replace(~r/eight/, line, "8")
-      line = Regex.replace(~r/nine/, line, "9")
-      # digits = List.flatten(digits)
-      IO.inspect(line)
-      digits = Regex.scan(~r/\d/, line, trim: true) # (&(for x <- &1, do: String.to_integer(x))).()
-      digits = List.flatten(digits)
+      IO.inspect("parsing line: " <> line)
+      digits = Enum.map(0..String.length(line) - 1, fn(index) ->
+        matched_num = Enum.find(Map.keys(num_map), fn (k) ->
+          key_length = String.length(k)
+          sub_string =  String.slice(line, index, key_length)
+          sub_string == k
+        end)
+        val = num_map[matched_num]
+        val
+      end) |> Enum.filter(fn(ele) -> ele != nil end)
+      IO.inspect("result:")
       IO.inspect(digits)
-      # digits_to_int = Enum.map(digits, fn(x) ->
-      #    {int, rest} = Integer.parse((x))
-      #    int
-      # end)
-      # Enum.map(&1, &(Integer.parse(&1)))
-      # acc + Enum.at(digits, 0) + List.last(digits)
-      # {first, _rest1} = Integer.parse(List.first(digits))
-      {num, _rest2} = Integer.parse(List.first(digits) <> List.last(digits))
-      # IO.inspect(num)
-      # IO.inspect(first)
-      # IO.inspect(last)
+      {num, _rest2} = Integer.parse(Integer.to_string(List.first(digits)) <> Integer.to_string(List.last(digits)))
       acc + num
     end)
     IO.inspect(sum)
